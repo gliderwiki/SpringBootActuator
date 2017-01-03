c9685843ae54d62d60fd7bc7f0e91f203a5233a2
yionlee <performizer@gmail.com>
2017-01-03 22:01:13 +0900

    chapter5 예제 추가

---
+++ src/main/java/com/libqa/web/login/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.libqa.web.login.service;
+
+import com.libqa.entity.Role;
+import com.libqa.entity.User;
+import com.libqa.web.login.repository.RoleRepository;
+import com.libqa.web.login.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+
+}
